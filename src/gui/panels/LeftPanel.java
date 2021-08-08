c03df200fb730b32cb450045a21f77a3d106d0d4
Felipe Flohr <felipeflohrlol@gmail.com>
2021-08-08 11:46:11 -0300

    Added start, sttegings and restart button

--- src/gui/panels/LeftPanel.java
+++ src/gui/panels/LeftPanel.java
@@ -21,6 +21,9 @@ public class LeftPanel extends JPanel implements ActionListener {
     int panel_height = 100;
     JFrame frame;
     JButton leaderButton;
+    JButton startButton;
+    JButton settingsButton;
+    JButton restartButton;
 
     public LeftPanel(JFrame frame){
         // Constructing variables
@@ -32,15 +35,43 @@ public class LeftPanel extends JPanel implements ActionListener {
         this.setOpaque(true);
         this.setLayout(new FlowLayout(FlowLayout.CENTER, 1, 30));
 
+        // Creating the start game button
+        startButton = new JButton();
+        startButton.setBorder(BorderFactory.createEmptyBorder());
+        startButton.setContentAreaFilled(false);
+        startButton.setIcon(new ImageIcon(ResizeImage.resizeImage("src/gui/images/plus.png", 32, 32))); // Placeholder
+        startButton.setFocusable(false);
+        startButton.addActionListener(this);
+
+        // Creating the restart button
+        restartButton = new JButton();
+        restartButton.setBorder(BorderFactory.createEmptyBorder());
+        restartButton.setContentAreaFilled(false);
+        restartButton.setIcon(new ImageIcon(ResizeImage.resizeImage("src/gui/images/restart.png", 32, 32))); // Placeholder
+        restartButton.setFocusable(false);
+        restartButton.addActionListener(this);
+
         // Creating the leaderboard button
         leaderButton = new JButton();
         leaderButton.setBorder(BorderFactory.createEmptyBorder());
         leaderButton.setContentAreaFilled(false);
-        leaderButton.setIcon(new ImageIcon(ResizeImage.resizeImage("src/gui/images/trophy.png", 50, 50)));
+        leaderButton.setIcon(new ImageIcon(ResizeImage.resizeImage("src/gui/images/trophy.png", 50, 50))); // Placeholder
+        leaderButton.setFocusable(false);
         leaderButton.addActionListener(this);
 
+        // Creating the settings button
+        settingsButton = new JButton();
+        settingsButton.setBorder(BorderFactory.createEmptyBorder());
+        settingsButton.setContentAreaFilled(false);
+        settingsButton.setIcon(new ImageIcon(ResizeImage.resizeImage("src/gui/images/settings.png", 32, 32))); // Placeholder
+        settingsButton.setFocusable(false);
+        settingsButton.addActionListener(this);
+
         // Adding components to the panel
+        this.add(startButton);
+        this.add(restartButton);
         this.add(leaderButton);
+        this.add(settingsButton);
 
         // Adding panel to the frame
         frame.add(this, BorderLayout.WEST);
