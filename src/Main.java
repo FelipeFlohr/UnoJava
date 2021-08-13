a0ad392e3542883fe168ec23308d13ebdbe59676
Felipe Flohr <felipeflohrlol@gmail.com>
2021-08-12 23:18:18 -0300

    Create a method for storing each deck of cards the player have

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		new Frame();
+		//new Frame();
+		System.out.println(Decks.getCurrentPlayerDecks());
 
 	}
 
--- src/functions/Decks.java
+++ src/functions/Decks.java
@@ -1,15 +1,17 @@
 package functions;
 
+import gui.panels.centerpanel.left.CenterCenterLeftPanel;
+
 import java.util.ArrayList;
 import java.util.Random;
 
+import static gui.panels.centerpanel.left.CenterCenterLeftPanel.GRID_COLS;
+import static gui.panels.centerpanel.left.CenterCenterLeftPanel.GRID_ROWS;
+
 public class Decks {
 
-    public static ArrayList<ArrayList<ArrayList<String>>> getGameDeck() {
-        return gameDeck;
-    }
-
     private static ArrayList<ArrayList<ArrayList<String>>> gameDeck = generateInitialDeck();
+    private static ArrayList<ArrayList<ArrayList<String>>> currentPlayerDecks = generateCurrentPlayerDividedDecks();
 
     // Will create a 2D ArrayList to store a player deck
     public static ArrayList<ArrayList<String>> generateDeck(){
@@ -30,13 +32,58 @@ public class Decks {
         return deck;
     }
 
-    // Will create a 3D ArrayList to store all players functions
+    // Will create a 3D ArrayList to store all players deck
     public static ArrayList<ArrayList<ArrayList<String>>> generateInitialDeck(){
         ArrayList<ArrayList<ArrayList<String>>> decks = new ArrayList<>();
         for(int i = 0; i < GlobalDefs.NUMBER_OF_PLAYERS; i++){
             decks.add(Decks.generateDeck());
         }
         return decks;
+    }
+
+    public static ArrayList<ArrayList<ArrayList<String>>> generateCurrentPlayerDividedDecks(){
+        int gridSize = GRID_ROWS * GRID_COLS;
+        ArrayList<ArrayList<String>> currentPlayerDeck = getGameDeck().get(0);
+        ArrayList<ArrayList<ArrayList<String>>> allDecks = new ArrayList<>();
+        int amountOfPages = GlobalDefs.totalAmountOfPages;
+
+        for(int i = 0; i < amountOfPages; i++){
+            ArrayList<ArrayList<String>> currentDeck = new ArrayList<>();
+            int iterationLimit;
+
+            // This will define how many cards must be iterated
+            if(amountOfPages == 1){
+                iterationLimit = currentPlayerDeck.size();
+            }
+            else if(i != (amountOfPages - 1)){
+                iterationLimit = gridSize * (i + 1);
+            }
+            else{
+                iterationLimit = (i * gridSize) + (currentPlayerDeck.size() - (i * gridSize));
+            }
+
+            System.out.println("Iteration: "+i+" | Starting from: "+(i * gridSize)+" | Until: "+iterationLimit);
+
+            for(int j = (i * gridSize); j < iterationLimit; j++){
+                currentDeck.add(currentPlayerDeck.get(j));
+            }
+            System.out.println(currentDeck);
+            allDecks.add(currentDeck);
+        }
+
+        return allDecks;
+    }
+
+    public static int generateTotalAmountOfPages(){
+        return (int) Math.ceil((double) Decks.getGameDeck().get(GlobalDefs.PLAYER_ID).size() / (double) (CenterCenterLeftPanel.GRID_ROWS * CenterCenterLeftPanel.GRID_COLS));
+    }
+
+    public static ArrayList<ArrayList<ArrayList<String>>> getGameDeck() {
+        return gameDeck;
+    }
+
+    public static ArrayList<ArrayList<ArrayList<String>>> getCurrentPlayerDecks() {
+        return currentPlayerDecks;
     }
 
 }
