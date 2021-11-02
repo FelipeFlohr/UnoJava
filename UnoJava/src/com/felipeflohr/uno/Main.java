c6d29f659b5c6f531a6a0e63ec1fbc381916aeb5
Felipe Flohr <felipeflohrlol@gmail.com>
2021-11-02 10:48:20 -0300

    Removed unused import

--- UnoJava/src/com/felipeflohr/uno/Main.java
+++ UnoJava/src/com/felipeflohr/uno/Main.java
@@ -2,8 +2,6 @@ package com.felipeflohr.uno;
 
 import com.felipeflohr.uno.swing.MainFrame;
 
-import static com.felipeflohr.uno.globaldefs.GlobalDefinitions.getTable;
-
 public class Main {
 
     public static void main(String[] args) {
