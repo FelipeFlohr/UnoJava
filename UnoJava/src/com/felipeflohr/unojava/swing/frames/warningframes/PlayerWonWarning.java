d798e346bb539e4cc401c18d7da68d2f83198f4d
Felipe Flohr <felipeflohrlol@gmail.com>
2021-11-20 02:09:55 -0300

    Fixed grammar

--- UnoJava/src/com/felipeflohr/unojava/swing/frames/warningframes/PlayerWonWarning.java
+++ UnoJava/src/com/felipeflohr/unojava/swing/frames/warningframes/PlayerWonWarning.java
@@ -8,7 +8,7 @@ import static com.felipeflohr.unojava.globaldefs.GlobalDefinitions.restartTable;
 public class PlayerWonWarning {
 
     public PlayerWonWarning() {
-        final String MESSAGE = "Player #" + getTable().getPlayerTurn() + " won! Do you want to start a new game? (Closing this window or pressing yes will close the game)";
+        final String MESSAGE = "Player #" + getTable().getPlayerTurn() + " won! Do you want to start a new game? (Closing this window or pressing no will close the game)";
         final String TITLE = "Player #" + getTable().getPlayerTurn() + "won!";
 
         int dialog = JOptionPane.showConfirmDialog(null, MESSAGE, TITLE, JOptionPane.YES_NO_OPTION);
